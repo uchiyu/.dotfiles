24790bf06cd910fbf5d493b5103b30452250bd8e
uchiyu <s13t208@stmail.eng.kagawa-u.ac.jp>
2015-10-26 22:23:02 +0900

    add vim and emacs setting

--- install.sh
+++ install.sh
@@ -10,7 +10,9 @@ source ~/.zshrc
 #emacs
 cd ~
 git clone https://github.com/uchiyu/.emacs.d.git
+ln -si ~/.emacs.d/init.el ~/
 
 #vim
 cd ~
 git clone https://github.com/uchiyu/.vim.git
+ln -si ~/.vim/.vimrc ~/
