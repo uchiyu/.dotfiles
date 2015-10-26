a38fce124adffd73957a2bbeafbc1012033f86ae
uchiyu <s13t208@stmail.eng.kagawa-u.ac.jp>
2015-10-26 21:45:31 +0900

    install default setting

--- install.sh
+++ install.sh
@@ -5,5 +5,12 @@ ln -si ~/.dotfiles/.tmux.conf ~/
 
 #zsh
 ln -si ~/.dotfiles/.zshrc ~/
+source ~/.zshrc
 
-#
+#emacs
+cd ~
+git clone https://github.com/uchiyu/.emacs.d.git
+
+#vim
+cd ~
+git clone https://github.com/uchiyu/.vim.git
